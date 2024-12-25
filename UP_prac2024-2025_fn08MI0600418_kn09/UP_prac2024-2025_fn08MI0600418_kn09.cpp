a42181331c4b85616e8999b0350450c9ca438ad0
stanev <yasenstanev3@gmail.com>
2024-12-25 13:22:33 +0200

    Created Main Function RegisterOrLogin
    Тази функция стимулира потребителя да избере една от опциите : да се регистрира или да влезе в съществуващ профил. Съответната опция го изпраща в нова функция, която осъществява избраната опция.

--- UP_prac2024-2025_fn08MI0600418_kn09/UP_prac2024-2025_fn08MI0600418_kn09.cpp
+++ UP_prac2024-2025_fn08MI0600418_kn09/UP_prac2024-2025_fn08MI0600418_kn09.cpp
@@ -1,11 +1,60 @@
 // UP_prac2024-2025_fn08MI0600418_kn09.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+//Solution to course project #9
+//Introduction to programming course
+//Faculty of Mathematics and Informatics of Sofia University
+//Winter Semester 2024 / 2025
+//
+//@author Yasen Stanev
+//@idnumber 08MI0600418
+//@compiler VC
+//
+//This project is designed for everyone who wants to track his / her daily calories and stuff.
+
 
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//1 - Register or log-in window
+void RegisterOrLogIn();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//����������� ��� ���� �� ����������
+	RegisterOrLogIn();
+}
+//Register or log-in window
+void RegisterOrLogIn()
+{
+	cout << "- - - Welcome to myfitnesspal! - - -" << endl << endl;
+	do
+	{
+		cout << "Please choose 1 or 2" << endl;
+		cout << "1. Log in"<<endl;
+		cout << "2. Sign up"<<endl;
+		cout << "Choice: ";
+		char option;
+		cin >> option;
+		if (option-'0' == 1)
+		{
+			LogInWindow();
+			break;
+		}
+		else if(option- '0' == 2)
+		{
+			RegisterWindow();
+			break;
+		}
+		else
+		{
+			cout << "! Invalid choice !"<<endl;
+		}
+	} while (true);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
