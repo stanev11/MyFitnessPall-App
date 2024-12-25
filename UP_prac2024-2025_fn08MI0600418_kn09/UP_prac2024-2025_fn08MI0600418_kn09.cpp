54de427b717752982ce95aba40cb3c1b0435c442
stanev <yasenstanev3@gmail.com>
2024-12-25 16:21:22 +0200

    Modified Function RegisterOrLogIn
    Промяна в валидирането на входните данни. Използвайки вградените функции на конзолата правим по-добро валидиране на входните данни, като не допускаме нищо друго освен променливи от тип целочислен.

--- UP_prac2024-2025_fn08MI0600418_kn09/UP_prac2024-2025_fn08MI0600418_kn09.cpp
+++ UP_prac2024-2025_fn08MI0600418_kn09/UP_prac2024-2025_fn08MI0600418_kn09.cpp
@@ -17,7 +17,6 @@
 #include <fstream>
 #include <vector>
 #include <string>
-
 using namespace std;
 
 //1 - Register or log-in window
@@ -25,7 +24,7 @@ void RegisterOrLogIn();
 
 int main()
 {
-	//����������� ��� ���� �� ����������
+	//1 - Register or log-in window
 	RegisterOrLogIn();
 }
 //Register or log-in window
@@ -38,14 +37,20 @@ void RegisterOrLogIn()
 		cout << "1. Log in"<<endl;
 		cout << "2. Sign up"<<endl;
 		cout << "Choice: ";
-		char option;
+		int option;
 		cin >> option;
-		if (option-'0' == 1)
+		if (cin.fail() || (option!=1 && option!=2))
+		{
+			cin.clear();
+			cin.ignore();
+			cout << "Invalid choice!" << endl;
+		}
+		else if (option == 1)
 		{
 			LogInWindow();
 			break;
 		}
-		else if(option- '0' == 2)
+		else
 		{
 			RegisterWindow();
 			break;
@@ -60,10 +65,3 @@ void RegisterOrLogIn()
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
